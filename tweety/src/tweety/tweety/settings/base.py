c82e86250b576bbc201ed2b34342e2292fbd5b84
Anup Tiwari <anup.tiwari39@gmail.com>
2019-07-04 10:36:07 +0530

    added django-rest-framework

--- tweety/src/tweety/tweety/settings/base.py
+++ tweety/src/tweety/tweety/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'rest_framework',
     'tweets',
 ]
 
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
